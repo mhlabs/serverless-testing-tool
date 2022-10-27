362463bdb7129a86c52af065aa91d7e1c4020010
Viggo Gustavii <viggo.gustavii@mathem.se>
2022-10-27 08:59:34 +0200

    Get method from queryParams

--- backend/src/GetMock.js
+++ backend/src/GetMock.js
@@ -4,7 +4,7 @@ const dynamoDb = new DynamoDB.DocumentClient();
 exports.handler = async function (event, context) {
 	const item = await dynamoDb.get({
 		TableName: process.env.TableName,
-		Key: { Path: event.path, Method: event.httpMethod }
+		Key: { Path: event.path, Method: event.queryStringParameters.httpMethod }
 	}).promise();
 
     return {
